c41c7a6c1621e343d7dc19ebb13b9ac4dcff1a5c
Alexander <5joura65@solent.ac.uk>
2024-01-10 17:08:08 +0000

    Added function to solve with backtracking

---
+++ backtrack.py
@@ -0,0 +1,62 @@
+def print_sudoku(puzzle):
+    print("-" * 25)
+    for a, row in enumerate(puzzle):
+        if a % 3 == 0 and a != 0:
+            print("-" * 25)
+        print("|", end=" ")
+        for b, num in enumerate(row):
+            if b % 3 == 0 and b != 0:
+                print("|", end=" ")
+            print(num, end=" ")
+        print("|")
+    print("-" * 25)
+
+
+# checks if number placment is valid
+def is_correct(puzzle, row, column, number):
+    # this checks the row and column the number is in
+    for num in range(9):
+        if puzzle[row][num] == number or puzzle[num][column] == number:
+            return False
+
+    # this checks the grid for the number
+    row_start = 3*(row//3)
+    column_start = 3*(column//3)
+    for a in range(3):
+        for b in range(3):
+            if puzzle[row_start+a][column_start+b] == number:
+                return False
+
+    return True
+
+
+# looks for empty spaces on the puzzle
+def look_for_blanks(puzzle):
+    for a in range(9):
+        for b in range(9):
+            if puzzle[a][b] == 0:
+                return a, b
+    return None
+
+
+def solve_sudoku(puzzle):
+    gap = look_for_blanks(puzzle)
+
+    if not gap:
+        return True
+
+    row, column = gap
+
+    for number in range(1, 10):
+        if is_correct(puzzle, row, column, number):
+            puzzle[row][column] = number
+
+            if solve_sudoku(puzzle):
+                return True
+
+            # backtrack for if the number is incorrect
+            puzzle[row][column] = 0
+
+    # if there's no number that can be placed backtrack again
+    return False
+
